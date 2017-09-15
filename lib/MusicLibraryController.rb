cacaa02aabfd59ee207cdfe900e4db27ad8e1fda
Robert Laws <roblaws@me.com>
2017-09-15 18:09:37 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -23,6 +23,16 @@ class MusicLibraryController
       input = gets.chomp
       if input == "list_songs"
         list_songs
+      elsif input == "list_artists"
+        list_artists
+      elsif input == "list_genres"
+        list_genres
+      elsif input == "list_songs_by_artist"
+        list_songs_by_artist
+      elsif input == "list_songs_by_genre"
+        list_songs_by_genre
+      elsif input == "play_song"
+        play_song
       end
     end
   end
@@ -34,19 +44,47 @@ class MusicLibraryController
   end
 
   def list_artists
-    old_files = @files
-    ff = old_files.collect do |f|
-      f.split(".")[0]
+    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
+      puts "#{i}. #{a.name}"
+    end
+  end
+
+  def list_genres
+    Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+      puts "#{i}. #{g.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.name} - #{s.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.artist.name} - #{s.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+
+    input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
     end
 
-    f = ff.collect do |f|
-      f.split(" - ")[0]
-    end
-
-    f = f.uniq
-
-    f = f.sort
-
-    f.each_with_index { |file, index| puts "#{index + 1}. #{file}" }
+    puts "Playing #{song.name} by #{song.artist.name}" if song
   end
 end
